0d35befd25594dc09e258843fd02cb4c5f2be878
Adam Allison <allisonadam81@gmail.com>
2022-09-12 12:32:49 -0700

    converted to react 18

--- client/login/SignUpText.jsx
+++ client/login/SignUpText.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const SignUpText = props => (
-  <p>Alternatively, you could </p>
+  <p>Alternatively, you could not I suppose?</p>
 )
 
 export default SignUpText
--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import App from './App.jsx';
-import { render } from 'react-dom';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 
 import styles from './application.scss'
 
-render(
-    <App></App>,
-  document.querySelector('#root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App/>);
 
